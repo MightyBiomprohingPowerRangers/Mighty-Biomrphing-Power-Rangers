425ca4ea08fef42c8e321c775be0604419f08eee
Kunal Pattni <pattnik@aston.ac.uk>
2015-02-28 21:12:53 +0000

    Bug fix 4
    Prevented the brush from going beyond the canvas boundary

--- Grow.java
+++ Grow.java
@@ -7,7 +7,7 @@ public class Grow
 {
 	Canvas c;
 	Random r;
-	
+
 	public Canvas Grow(Canvas c) 
 	{
 		this.c = c;
@@ -15,28 +15,31 @@ public class Grow
 		moveBrush();
 		return c;
 	}
-	
+
 	public void moveBrush(/*int angle, int magnitude*/)
 	{
 		drawSLines();
 	}
-	
+
 	public void drawLine(int cycles)
 	{	
 		int x = 250;
 		int y = 250;
 		for (int i = 1; i < cycles + 1; i++)
 		{
-			x+= r.nextInt(11) - 5;
-			y+= r.nextInt(11) - 5;
+			if (x > 10 && x < 490 && y > 10 && y < 490)
+			{
+				x+= r.nextInt(11) - 5;
+				y+= r.nextInt(11) - 5;
+			}
 			c.drawPoint(x, y);
 		}
 	}
-	
+
 	public void drawSLines()
 	{
 		drawLine(5000);
 	}
-	
+
 
 }
