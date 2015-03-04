fa375706f49cdbb05de99f1105bfea622d8a39ef
Kunal Pattni <pattnik@aston.ac.uk>
2015-03-04 23:11:07 +0000

    Made a new abstract class Unique
    this allows us to make clusters with chosen features which can then be
    applied to the canvas

--- Unique.java
+++ Unique.java
@@ -1,11 +1,15 @@
 package MBPR;
 
-public class Unique extends Cluster 
+import java.util.Random;
+
+public abstract class Unique extends Cluster 
 {
-
-	public Unique() 
+	protected int maxX;
+	protected int maxY;
+	public Unique(int maxX, int maxY) 
 	{
-
+		this.maxX = maxX;
+		this.maxY = maxY;
 	}
 	
 	public void addPoint(int x, int y)
