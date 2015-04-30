d7855955bc3efb9f4b02e3f845dd30162701be2b
Kunal Pattni <pattnik@aston.ac.uk>
2015-04-30 13:09:32 +0100

    Added a Test class
    Just to be used to test specific methods

---
+++ Test.java
@@ -0,0 +1,18 @@
+package MBPR;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int[] array = Canvas.getARGBarray(Canvas.getARGBValue(200, 4, 5, 100));
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+		System.out.println(array[2]);
+		System.out.println(array[3]);
+
+	}
+
+}
